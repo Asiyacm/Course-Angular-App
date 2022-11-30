322eb54c904740e8de35bacb7cfcbe4f212c56fd
Asiyacm <asiyacmuhammed@gmail.com>
2022-11-30 09:57:22 +0530

    apiDataAdd

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -24,22 +24,23 @@ constructor(private api:ApiService){}
   {"courseTitle":this.courseTitle,"courseDescription":this.courseDescription, "courseDuration":this.courseDuration,"courseDate":this.courseDate,"courseVenue":this.courseVenue}
 console.log(data)
 
-this.api.addCourse(data).subscribe(
-(response:any)=>{
 
-  console.log(response)
-  if(response.status=="success") {
-    alert("Add Course Successfully")
-    this.courseTitle=""
-    this.courseDescription=""
-    this.courseDuration=""
-    this.courseDate=""
-    this.courseVenue=""
-    
-  } else {
-    
-    alert("Something Went Wrong")
-  }
+this.api.addCourse(data).subscribe(
+  (response:any)=>{
+  
+    console.log(response)
+    if(response.status=="success") {
+      alert("Add Course Successfully")
+      this.courseTitle=""
+      this.courseDescription=""
+      this.courseDuration=""
+      this.courseDate=""
+      this.courseVenue=""
+      
+    } else {
+      
+      alert("Something Went Wrong")
+    }
 }
 
 
